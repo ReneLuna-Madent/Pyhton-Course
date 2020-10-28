7629deaa623404b56bb50fc966433432c8298795
Rene Luna <reneworks25@gmail.com>
2020-10-28 01:05:23 -0600

    Convertidor de Pesos a dolares en Python

---
+++ conversor.py
@@ -0,0 +1,7 @@
+pesos = input("¿Cuantos pesos Tienes?: ")
+pesos = float(pesos)
+valor_dolar = 21.04
+dolares = pesos / valor_dolar
+dolares = round(dolares, 2)
+dolares =str(dolares)
+print("Tienes: $" + dolares + " USD")
