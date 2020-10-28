e41f9475cf4a691d1be63652cc5df4f3e8b75c45
Rene Luna <reneworks25@gmail.com>
2020-10-28 01:59:48 -0600

    Convertidor de dolares a pesos en Python

---
+++ conversor usdtomnx.py
@@ -0,0 +1,8 @@
+USD = input("¿Cuantos USD Tienes?: ")
+USD = float(USD)
+valor_pesomnx = 21.04
+pesos = USD * valor_pesomnx
+pesos = round(dolares, 2)
+pesos =str(dolares)
+print("Tienes: $" + pesos + " USD")
+print ("Prueba Github")
