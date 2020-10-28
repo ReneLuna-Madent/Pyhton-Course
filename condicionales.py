1d7038955f7669d137f7f2dcb7598bb5c8b468ec
Rene Luna <reneworks25@gmail.com>
2020-10-28 02:30:02 -0600

    cambio en programa de condicionles

--- condicionales.py
+++ condicionales.py
@@ -1,5 +1,14 @@
-edad = int(input("Escribe tu edad: "))
-if edad > 17:   
-     print("Eres Mayor de Edad")
-else:
-    print("Eres menor de Edad")
+# edad = int(input("Escribe tu edad: "))
+# if edad > 17:   
+#     print("Eres Mayor de Edad")
+# else:
+#    print("Eres menor de Edad")
+
+numero = int(input("escribe un numero)"))
+
+if numero > 5:    
+    print("Es mayor a Cinco")
+elif numero == 5:     
+    print("Es igual a Cinco")
+else:    
+    print("Es menor a Cinco")
