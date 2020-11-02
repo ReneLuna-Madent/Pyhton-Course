1d5c6f9cb2e52fb76df9e562aa4d8a96d073c23f
Rene Luna <reneworks25@gmail.com>
2020-11-02 00:43:27 -0600

    Se cambia a string la variable contador

--- Ejercicioscodigo/bucles.py
+++ Ejercicioscodigo/bucles.py
@@ -3,10 +3,11 @@ def run():
     contador = 0
     potencia_2 = 2**contador
     while potencia_2 < LIMITE:
-        print("2 elevado a " + str(contador) + "Es igual a: " + potencia_2)
+        print("2 elevado a " + str(contador) + "Es igual a: " + str(potencia_2))
+
         contador = contador + 1
         potencia_2 = 2**contador
 
-        
+
 if __name__ == "__main__":
     run()
