1d53edc8505274e1d7d544bd70c22fc24ee8b3a2
Rene Luna <reneworks25@gmail.com>
2020-11-02 15:52:43 -0600

    se agrega programa recorrer

---
+++ Ejercicioscodigo/recorrer.py
@@ -0,0 +1,9 @@
+def run():
+    nombre = input("Escribe tu Frase: ")
+    for letra in nombre:
+        print(letra.upper()) #se agrega upper para imprimir en mayus
+
+
+if __name__ == "__main__" :
+    run()
+
