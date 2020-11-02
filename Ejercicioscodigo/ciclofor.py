b8aa4447b98152cda1e2330fba7f23cec27135ec
Rene Luna <reneworks25@gmail.com>
2020-11-02 15:33:14 -0600

    se agrega ejercicio de ciclo for

---
+++ Ejercicioscodigo/ciclofor.py
@@ -0,0 +1,12 @@
+contador = 1
+print(contador)
+while contador < 1000:
+    contador = contador + 1
+    # contador += 1  / es igual a la funcion superior
+    print(contador)
+
+a = list(range(range(1000)) #list imprime los valores de manera consecuente
+print(a)
+
+for contador in range(1, 1001): #al usar dos variables se indica el valor inicial y el final -1
+    print(contador)
